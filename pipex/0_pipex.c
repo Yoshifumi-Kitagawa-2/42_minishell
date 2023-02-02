d5c1e2b93ca361168cd41f7a1907053a85ca92cd
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2023-02-02 23:46:50 +0900

    commit

---
+++ pipex/0_pipex.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   0_pipex.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: KitagawaYoshifumi <KitagawaYoshifumi@st    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/02 23:08:04 by yokitaga	       #+#    #+#             */
+/*   Updated: 2023/02/02 23:08:05 by yokitaga	      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+/*__attribute__((destructor))
+static void destructor() {
+    system("leaks -q pipex");
+}*/
+
+int main(int argc, char **argv, char **envp)
+{
+	
+	return (0);
+}
